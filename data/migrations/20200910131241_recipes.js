9636b870be77f9980b1ad88e7d8d06437c3a5b15
Galo Sandoval <galosan92@gmail.com>
2020-09-10 14:17:15 -0700

    finished the migrations

---
+++ data/migrations/20200910131241_recipes.js
@@ -0,0 +1,42 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("ingredients", (tbl) => {
+      tbl.increments();
+
+      tbl.text("ingredient_name", 20).notNullable().unique();
+    })
+    .createTable("steps", (tbl) => {
+      tbl.increments();
+
+      tbl.text("Description", 128).notNullable();
+    })
+    .createTable("recipes", (tbl) => {
+      tbl.increments();
+
+      tbl
+        // default foreign key methods
+        .integer("ingredients_id")
+        .unsigned() // support only positive numbers
+        .notNullable()
+        .references("ingredients.id")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+
+      tbl
+        .integer("step_id")
+        .unsigned()
+        .notNullable()
+        .references("steps.id")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+
+      tbl.float("amount_needed").unsigned().notNullable();
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("recipes")
+    .dropTableIfExists("steps")
+    .dropTableIfExists("ingredients");
+};
